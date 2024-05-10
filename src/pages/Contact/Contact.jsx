849421051012fb2962edb84bc139e5c7c28929c5
rakotorisonlandry <rakotorisonlandry@gmail.com>
2024-05-10 09:52:01 +0200

    Add Formulaire

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
-
+import { Box } from "@mui/material";
+import Form from "./Form";
+import React from "react";
+import Typography from "@mui/material";
+import { Link } from "react-router-dom";
 function Contact() {
   return (
-    <div>Contact</div>
-  )
+    <Box>
+      <Typography variant="H2" className="contact-Title">Contact Me</Typography>
+      <Box className="Form-contact-container">
+        <Box>
+          <img src="" alt="" />
+          <Typography>RAKOTOARISON Tsaraefadahy Landry Brigea</Typography>
+          <Typography>I am available for freelance work. Connect wirh me via Email and call in to my account</Typography>
+          <Typography>Phone <span>+261340508180</span></Typography>
+          <Typography>Emails <span>Rakotorisonlandry@gmail.com</span></Typography>
+          <Typography>FIND ME WITH</Typography>
+          <Box>
+            <Link></Link>
+            <Link></Link>
+            <Link></Link>
+          </Box>
+        </Box>
+        <Box>
+          <Form/>
+        </Box>
+      </Box>
+    </Box>
+  );
 }
 
-export default Contact
+export default Contact;
