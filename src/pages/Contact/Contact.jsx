85596411e48b3114e60a68d67e975389dd055679
rakotorisonlandry <rakotorisonlandry@gmail.com>
2024-05-12 09:09:54 +0200

    add contact page

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,19 +1,28 @@
 import { Box } from "@mui/material";
 import Form from "./Form";
 import React from "react";
-import Typography from "@mui/material";
+import { Typography } from "@mui/material";
 import { Link } from "react-router-dom";
 function Contact() {
   return (
     <Box>
-      <Typography variant="H2" className="contact-Title">Contact Me</Typography>
+      <Typography variant="H2" className="contact-Title">
+        Contact Me
+      </Typography>
       <Box className="Form-contact-container">
         <Box>
           <img src="" alt="" />
           <Typography>RAKOTOARISON Tsaraefadahy Landry Brigea</Typography>
-          <Typography>I am available for freelance work. Connect wirh me via Email and call in to my account</Typography>
-          <Typography>Phone <span>+261340508180</span></Typography>
-          <Typography>Emails <span>Rakotorisonlandry@gmail.com</span></Typography>
+          <Typography>
+            I am available for freelance work. Connect wirh me via Email and
+            call in to my account
+          </Typography>
+          <Typography>
+            Phone <span>+261340508180</span>
+          </Typography>
+          <Typography>
+            Emails <span>Rakotorisonlandry@gmail.com</span>
+          </Typography>
           <Typography>FIND ME WITH</Typography>
           <Box>
             <Link></Link>
@@ -22,7 +31,7 @@ function Contact() {
           </Box>
         </Box>
         <Box>
-          <Form/>
+          <Form />
         </Box>
       </Box>
     </Box>
