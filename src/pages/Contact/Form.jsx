ae05f83f68f6bc007adc35b5ff3f6fe24221ab79
rakotorisonlandry <rakotorisonlandry@gmail.com>
2024-05-15 00:08:22 +0200

    last commit in this day

--- src/pages/Contact/Form.jsx
+++ src/pages/Contact/Form.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Box } from "@mui/material";
-
+import { Link } from "react-router-dom";
 function Form() {
   return (
     <Box className="Form-Container">
@@ -8,34 +8,61 @@ function Form() {
         <Box className="single-Form">
           <label htmlFor="name">Your Name</label>
           <Box>
-            <input className="input-form" type="text" id="name" placeholder="ex: Rakoto" />
+            <input
+              className="input-form"
+              type="text"
+              id="name"
+              placeholder="ex: Rakoto"
+            />
           </Box>
         </Box>
         <Box className="single-Form">
           <label htmlFor="phoneNumber">Phone Number</label>
           <Box>
-            <input className="input-form" type="text" id="phoneNumber" placeholder="xxxxxxxxxx" />
+            <input
+              className="input-form"
+              type="text"
+              id="phoneNumber"
+              placeholder="xxxxxxxxxx"
+            />
           </Box>
         </Box>
       </Box>
       <Box className="single-Form">
         <label htmlFor="email">Email</label>
         <Box>
-          <input className="input-form" type="text" id="email" placeholder="example@gmail.com" />
+          <input
+            className="input-form"
+            type="text"
+            id="email"
+            placeholder="example@gmail.com"
+          />
         </Box>
       </Box>
       <Box className="single-Form">
         <label htmlFor="subject">Subject</label>
         <Box>
-          <input className="input-form" type="text" id="subject" placeholder="Lorem" />
+          <input
+            className="input-form"
+            type="text"
+            id="subject"
+            placeholder="Lorem"
+          />
         </Box>
       </Box>
       <Box className="single-Form">
         <label htmlFor="message">Message</label>
         <Box>
-          <textarea className="input-form" id="message" placeholder="Lorem"></textarea>
+          <textarea
+            className="input-form-last"
+            id="message"
+            placeholder="Lorem"
+          ></textarea>
         </Box>
       </Box>
+      <Link className="send-Form">
+        Send
+      </Link>
     </Box>
   );
 }
