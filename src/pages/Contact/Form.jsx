7bcfb522a701a269a8301b1517afdbccb4ee5779
rakotorisonlandry <rakotorisonlandry@gmail.com>
2024-05-14 10:39:16 +0200

    Update Form

--- src/pages/Contact/Form.jsx
+++ src/pages/Contact/Form.jsx
@@ -1,15 +1,21 @@
-import { Box } from '@mui/material'
-import React from 'react'
+import { Box } from "@mui/material";
+import React from "react";
 
 function Form() {
   return (
     <Box>
       <Box>
-        <label htmlFor="name">Nom</label>
-        <input type="text" placeholder='Votre nom'/>
+        <Box>
+          <label htmlFor="name">Nom</label>
+          <input type="text" placeholder="Votre nom" />
+        </Box>
+        <Box>
+          <label htmlFor="firstName">Prenom</label>
+          <input type="text" placeholder="Votre Votre Prenom" />
+        </Box>
       </Box>
     </Box>
-  )
+  );
 }
 
-export default Form
+export default Form;
