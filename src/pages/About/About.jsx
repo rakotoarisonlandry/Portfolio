4a8c29a7b6ef3b3f2765c782fc4a60c1df330cfe
rakotorisonlandry <rakotorisonlandry@gmail.com>
2024-05-18 12:11:51 +0200

    Add Box shadow

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -5,7 +5,7 @@ import Landry from "../../../public/assets/img/landry.png";
 function About() {
   return (
     <Box className="ml-[350px] h-full">
-      <Box className="border-[#8720DF] overflow-hidden pt-7 border-[5px] rounded-[37px]">
+      <Box className="border-[#8720DF] shadow-lg shadow-[#8720DF]/50 overflow-hidden pt-7 border-[5px] rounded-[37px]">
         <img src={Landry} alt="" />
       </Box>
       <Box>
