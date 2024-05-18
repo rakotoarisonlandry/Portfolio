1651fcdc1db2fada0d577d50d3d19c3056e1d8d6
rakotorisonlandry <rakotorisonlandry@gmail.com>
2024-05-18 11:00:22 +0200

    Change about page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -5,7 +5,7 @@ function About() {
   return (
     <Box className="ml-[350px]">
       <Box className="border-[#8720DF] overflow-hidden pt-7 border-[5px] rounded-[37px]">
-        <img src={Landry} alt="" className="w-80" />
+        <img src={Landry} alt="" />
       </Box>
     </Box>
   );
