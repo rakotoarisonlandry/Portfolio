adfc66fed732a495a86535d76783a5e4cf075db9
rakotorisonlandry <rakotorisonlandry@gmail.com>
2024-05-18 10:58:12 +0200

    page about  added

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,10 +1,14 @@
-import { Box } from '@mui/material'
-import React from 'react'
-
+import { Box } from "@mui/material";
+import React from "react";
+import Landry from "../../../public/assets/img/landry.png";
 function About() {
   return (
-    <Box >About</Box>
-  )
+    <Box className="ml-[350px]">
+      <Box className="border-[#8720DF] overflow-hidden pt-7 border-[5px] rounded-[37px]">
+        <img src={Landry} alt="" className="w-80" />
+      </Box>
+    </Box>
+  );
 }
 
-export default About
+export default About;
