5946a4597cc0933bc96a70ad9a2d73e111020741
rakotorisonlandry <rakotorisonlandry@gmail.com>
2024-05-18 11:42:30 +0200

    HomePage updated

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -41,8 +41,8 @@ function Home() {
       </Box>
       <HomeComponent />
       {/* <Container> */}
-      <About/>
-        <Contact />
+      {/* <About/>
+        <Contact /> */}
       {/* </Container> */}
     </Box>
   );
