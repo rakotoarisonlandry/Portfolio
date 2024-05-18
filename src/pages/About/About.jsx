bd54f1716a636f6974bce7c1b1aa54e7e4f29bee
rakotorisonlandry <rakotorisonlandry@gmail.com>
2024-05-18 11:35:01 +0200

    Modif About

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,11 +1,15 @@
 import { Box } from "@mui/material";
 import React from "react";
+import {Typography} from "@mui/material";
 import Landry from "../../../public/assets/img/landry.png";
 function About() {
   return (
-    <Box className="ml-[350px]">
+    <Box className="ml-[350px] h-full">
       <Box className="border-[#8720DF] overflow-hidden pt-7 border-[5px] rounded-[37px]">
         <img src={Landry} alt="" />
+      </Box>
+      <Box>
+          
       </Box>
     </Box>
   );
