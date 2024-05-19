dd525df6d401deeac1dc241152a8eb73e16b50c5
rakotorisonlandry <rakotorisonlandry@gmail.com>
2024-05-19 06:52:14 +0200

    change style to tailwind css

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -9,44 +9,44 @@ import { Link } from "react-router-dom";
 import respect from "../../../public/assets/img/92f66ceb28916af6e1aeb8d5e8054f3c-1000x640.jpg";
 function Contact() {
   return (
-    <Box className="Contact-Container">
-      <Container className="container-Title">
-        <Typography variant="H2" className="contact-Title">
+    <Box className="ml-[350px] w-[140vh]">
+      <Container className="text-center">
+        <Typography variant="H2" className="">
           Contact Me
         </Typography>
       </Container>
-      <Box className="Form-contact-container">
-        <Box className="container-typography">
-          <Box className="Img-Contact">
-            <img src={respect} alt="" width="300px" height="170px" />
+      <Box className="flex ">
+        <Box className="w-[56vh] mr-32">
+          <Box className="rounded-[5px]">
+            <img src={respect} alt="" className="rounded-[5px]" width="300px" height="170px" />
           </Box>
-          <Typography className="name-Typography">
+          <Typography variant='Body1' className="font-extrabold">
             RAKOTOARISON Tsaraefadahy Landry Brigea
           </Typography>
-          <Typography variant="caption" className="caption-typo">
+          <Typography variant="caption" className="">
             I am available for freelance work. Connect with me via Email and
             call in to my account
           </Typography>
-          <Typography variant="body2" className="email-typo">
-            Phone: <span className="component-Number">+261340508180</span>
+          <Typography variant="body2" className="">
+            Phone: <span className="">+261340508180</span>
           </Typography>
-          <Typography variant="body2" className="email-typo">
+          <Typography variant="body2" className="">
             Emails:{" "}
-            <span className="component-Number">
+            <span className="">
               Rakotorisonlandry@gmail.com
             </span>
           </Typography>
-          <Typography variant="body2" className="find-typo">
+          <Typography variant="body2" className="">
             FIND ME WITH
           </Typography>
-          <Box className="social-Link">
-            <Link className="Link-component">
+          <Box className="">
+            <Link className="">
               <FaFacebookF />
             </Link>
-            <Link className="Link-component">
+            <Link className="">
               <FaLinkedinIn />
             </Link>
-            <Link to="https://github.com/rakotoarisonlandry" className="Link-component">
+            <Link to="https://github.com/rakotoarisonlandry" className="">
               <FiGithub />
             </Link>
           </Box>
