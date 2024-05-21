a1438e7e9dd6ba9ec11fa3be4c369529027930be
rakotorisonlandry <rakotorisonlandry@gmail.com>
2024-05-21 18:46:17 +0200

    simple modif

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 import { TypeAnimation } from "react-type-animation";
 import HomeComponent from "./HomeComponent";
 import Blur from "../../../public/assets/img/blur.svg";
+import { MdOutlinePermContactCalendar } from "react-icons/md";
 import hello from "../../../public/assets/img/stock-vector-hi-hello-banner-speech-bubble-poster-and-sticker-concept-with-text-hello-white-bubble-message-1505210795-removebg-preview.png";
 import portrait from "../../../public/assets/img/test.svg";
 import Contact from "../Contact/Contact";
@@ -16,7 +17,7 @@ function Home() {
         <Box className="Home-container">
           <img src={hello} alt="" className="Home-hello" />
           <Typography variant="h4" fontWeight="900" className="Home-Title">
-            <span >I'm</span> Landry RAKOTOARISON
+            <span>I'm</span> Landry RAKOTOARISON
           </Typography>
           <TypeAnimation
             sequence={["Developer", 1000, "Developer & Designer", 1000]}
@@ -34,7 +35,10 @@ function Home() {
             much as I enjoyed creating it.
           </Typography>
           <Link className="home-Link" to="/contact">
-            <button className="home-Button">Contact Me</button>
+            <button className="home-Button flex">
+              <MdOutlinePermContactCalendar className="text-[20px] mr-3"/>
+              Contact Me
+            </button>
           </Link>
         </Box>
         <img src={portrait} alt="" className="Home-portrait" />
