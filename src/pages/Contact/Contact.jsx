1c0e2540f0b53cce3734b5b918d2541826e0e769
rakotorisonlandry <rakotorisonlandry@gmail.com>
2024-05-21 18:45:17 +0200

    Contact updated

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -9,14 +9,17 @@ import { Link } from "react-router-dom";
 import respect from "../../../public/assets/img/92f66ceb28916af6e1aeb8d5e8054f3c-1000x640.jpg";
 function Contact() {
   return (
-    <Box className="ml-[350px] h-[100vh] w-[140vh]">
+    <Box className="Contact-Container ml-[360px] h-[100vh] w-[140vh] ">
       <Container className="text-center">
-        <Typography variant="h5" className="pb-32 pt-10 font-extrabold">
+        <Typography
+          variant="h5"
+          className="Contact-Title pb-32 pt-10 font-extrabold"
+        >
           Contact Me
         </Typography>
       </Container>
-      <Box className="flex ">
-        <Box className="w-[40vh] mr-48 text-justify">
+      <Box className="flex Content-Contact">
+        <Box className="w-[40vh] mr-48 text-justify info-Contact">
           <Box className="rounded-[5px] pb-3">
             <img
               src={respect}
@@ -27,31 +30,33 @@ function Contact() {
             />
           </Box>
           <Box className="pb-4">
-            <Typography variant="Body1" className="font-extrabold ">
+            <Typography variant="Body1" className="font-extrabold typo">
               RAKOTOARISON Tsaraefadahy Landry Brigea
             </Typography>
           </Box>
-          <Typography variant="body2" className="pb-3">
-            I am available for freelance work. Connect with me via Email and
-            call in to my account
+          <Box>
+            <Typography variant="body2" className="pb-3 typo">
+              I am available for freelance work. Connect with me via Email and
+              call in to my account
+            </Typography>
+          </Box>
+          <Typography variant="body2" className="">
+            Phone: <span className="typo">+261340508180</span>
           </Typography>
           <Typography variant="body2" className="">
-            Phone: <span className="">+261340508180</span>
+            Emails: <span className="typo">Rakotorisonlandry@gmail.com</span>
           </Typography>
-          <Typography variant="body2" className="">
-            Emails: <span className="">Rakotorisonlandry@gmail.com</span>
-          </Typography>
-          <Typography variant="body2" className="pt-3 pb-3">
+          <Typography variant="body2" className="pt-3 pb-3 typo">
             FIND ME WITH
           </Typography>
-          <Box className="flex space-x-4">
-            <Link className="">
+          <Box className="flex space-x-3">
+            <Link className="border-[1px] rounded-full p-3 border-[#9b46e4] hover:bg-[#9b46e4] hover:text-[#fff]">
               <FaFacebookF />
             </Link>
-            <Link className="">
+            <Link className="border-[1px] rounded-full p-3 border-[#9b46e4] hover:bg-[#9b46e4] hover:text-[#fff]">
               <FaLinkedinIn />
             </Link>
-            <Link to="https://github.com/rakotoarisonlandry" className="">
+            <Link to="https://github.com/rakotoarisonlandry" className="border-[1px] rounded-full p-3 border-[#9b46e4] hover:bg-[#9b46e4] hover:text-[#fff]">
               <FiGithub />
             </Link>
           </Box>
