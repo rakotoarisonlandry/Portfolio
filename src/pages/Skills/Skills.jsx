c0f3f137db903cce7b6509fc5f8a88d9041ef40a
rakotorisonlandry <rakotorisonlandry@gmail.com>
2024-05-23 21:53:49 +0200

    Add page

--- src/pages/Skills/Skills.jsx
+++ src/pages/Skills/Skills.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
-
+import { Box } from "@mui/material";
+import React from "react";
+import {Typography} from "@mui/material";
 function Skills() {
   return (
-    <div>Skills</div>
-  )
+    <Box className="flex">
+      <Box>
+        <Box>
+          <Typography></Typography>
+        </Box>
+        <Box></Box>
+        <Box></Box>
+        <Box></Box>
+      </Box>
+      <Box>
+        <img src="" alt="mycv" />
+      </Box>
+    </Box>
+  );
 }
 
-export default Skills
+export default Skills;
